8d9ff6176ebd2aaf69ba678ce1fc64cef94d0829
Misiaczek <Mrmisio345@gmail.com>
2022-06-08 15:10:02 +0200

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Clock
+{
+    partial class ClockForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ClockForm));
+            this.panel = new System.Windows.Forms.Panel();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.colorDialog = new System.Windows.Forms.ColorDialog();
+            this.SuspendLayout();
+            // 
+            // panel
+            // 
+            this.panel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel.Location = new System.Drawing.Point(98, 12);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(326, 326);
+            this.panel.TabIndex = 0;
+            this.panel.Paint += new System.Windows.Forms.PaintEventHandler(this.panel_Paint);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+
+            // 
+            // ClockForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 350);
+            this.Controls.Add(this.panel);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.ColorDialog colorDialog;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Text;
+using System.Windows.Forms;
+
+namespace Clock
+{
+    public partial class ClockForm : Form {
+        private Color _innerColor = Color.Aquamarine, _outerColor = Color.Aqua, _handColor = Color.Black, _secHandColor = Color.Red, _tickColor = Color.Black, _textColor = Color.Black;
+
+        private void timer_Tick(object sender, EventArgs e) {
+            this.InvokePaint(panel, new PaintEventArgs(Graphics.FromHwnd(panel.Handle), new Rectangle(panel.Location.X, panel.Location.Y, panel.Size.Width, panel.Size.Height)));
+        }
+
+        public ClockForm() {
+            InitializeComponent();
+            
+            if (!timer.Enabled) {
+                timer.Start();
+            }
+        }
+
+        private void panel_Paint(object sender, PaintEventArgs e) {
+            var gOne = e.Graphics;
+            BufferedGraphicsContext CurrentContext = BufferedGraphicsManager.Current;
+            BufferedGraphics g = CurrentContext.Allocate(gOne, ClientRectangle);
+            g.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            g.Graphics.TextRenderingHint = TextRenderingHint.AntiAlias;
+            g.Graphics.Clear(panel.BackColor);
+            StringFormat stringFormat = new StringFormat();
+            stringFormat.Alignment = StringAlignment.Center;
+            stringFormat.LineAlignment = StringAlignment.Center;
+            var Ox = panel.Width / 2;
+            var Oy = panel.Height / 2;
+            int sideSquare;
+
+            if (panel.Width > panel.Height) {
+                sideSquare = panel.Height;
+            } else {
+                sideSquare = panel.Width;
+            }
+
+            // elipse
+            GraphicsPath path = new GraphicsPath();
+            path.AddEllipse(new Rectangle(panel.Width / 20, panel.Height / 20, panel.Width - (panel.Width / 20) * 2, panel.Height - (panel.Height / 20) * 2));
+            PathGradientBrush pthGrBrush = new PathGradientBrush(path);
+            pthGrBrush.CenterColor = _innerColor;
+            Color[] colors = { _outerColor };
+            pthGrBrush.SurroundColors = colors;
+            g.Graphics.FillEllipse(pthGrBrush, new Rectangle(panel.Width / 20, panel.Height / 20, panel.Width - (panel.Width / 20) * 2, panel.Height - (panel.Height / 20) * 2));
+
+            // ticks
+            g.Graphics.DrawEllipse(new Pen(_tickColor), new Rectangle(panel.Width / 20, panel.Height / 20, panel.Width - (panel.Width / 20) * 2, panel.Height - (panel.Height / 20) * 2));
+            g.Graphics.TranslateTransform(Ox, Oy);
+
+            for (int i = 0; i < 4; i++) {
+                g.Graphics.DrawLine(new Pen(_tickColor, 6), new Point((panel.Width / 2 - panel.Width / 20) * -1, 0), new Point((panel.Width / 2 - panel.Width / 20) * -1 + 13, 0));
+                for (int j = 0; j < 15; j++)
+                {
+                    g.Graphics.DrawLine(new Pen(_tickColor, 1), new Point((panel.Width / 2 - panel.Width / 20) * -1, 0), new Point((panel.Width / 2 - panel.Width / 20) * -1 + 9, 0));
+                    if (j % 5 == 0)
+                        g.Graphics.DrawLine(new Pen(_tickColor, 4), new Point((panel.Width / 2 - panel.Width / 20) * -1, 0), new Point((panel.Width / 2 - panel.Width / 20) * -1 + 13, 0));
+                    g.Graphics.RotateTransform(6);
+                }
+            }
+
+            // Draw1
+            g.Graphics.ResetTransform();
+            g.Graphics.TranslateTransform(Ox, Oy);
+            double radius = ((panel.Width - panel.Width / 20.0 * 2) / 2 - 28);
+            float x0 = 0;
+            float y0 = -(float)radius;
+
+            for (int i = 1; i < 13; i++) {
+                if (i == 10 || i == 11) {
+                    y0 = -(float)(radius - 3);
+                }
+                var rx = x0 - 0;
+                var ry = y0 - 0;
+                var c = Math.Cos((30 * i) * Math.PI / 180);
+                var s = Math.Sin((30 * i) * Math.PI / 180);
+                float x1 = (float)(0 + rx * c - ry * s);
+                float y1 = (float)(0 + rx * s + ry * c);
+                g.Graphics.DrawString(i.ToString(), new Font("Calibri", 20, FontStyle.Bold), new SolidBrush(_textColor), x1, y1, stringFormat);
+            }
+
+            // Draw2
+            var angleSec = DateTime.Now.Second * 6;
+
+            g.Graphics.ResetTransform();
+            g.Graphics.TranslateTransform(Ox, Oy);
+            g.Graphics.RotateTransform(angleSec);
+            g.Graphics.DrawLine(new Pen(_secHandColor), 0, 30, 0, -133);
+
+            // Draw3
+            g.Graphics.ResetTransform();
+            g.Graphics.TranslateTransform(Ox, Oy);
+            var angleMin = DateTime.Now.Minute * 6;
+            var angleHour = DateTime.Now.Hour * 30 + 30.0 / 100.0 * (DateTime.Now.Minute / (60.0 / 100.0));
+
+            g.Graphics.FillEllipse(new SolidBrush(_handColor), -7, -7, 14, 14);
+            g.Graphics.RotateTransform(angleMin);
+            g.Graphics.FillPolygon(new SolidBrush(_handColor), new Point[]{new Point(0, 0), new Point(5, -30), new Point(0, -105), new Point(-5, -30)});
+            g.Graphics.ResetTransform();
+            g.Graphics.TranslateTransform(Ox, Oy);
+            g.Graphics.RotateTransform((float)angleHour);
+            g.Graphics.FillPolygon(new SolidBrush(_handColor), new Point[] { new Point(0, 0), new Point(5, -24), new Point(0, -75), new Point(-5, -24) });
+
+            g.Graphics.FillEllipse(new SolidBrush(_secHandColor), -4, -4, 8, 8);
+
+            g.Render();
+            g.Dispose();
+        }
+    }
+}
